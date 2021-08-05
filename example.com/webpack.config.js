698b4255288a0a2c10d744ca34840a644475277b
tamakiii <tamakiii@users.noreply.github.com>
2021-08-05 11:38:41 +0900

    fixes

--- example.com/webpack.config.js
+++ example.com/webpack.config.js
@@ -1,15 +1,9 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
+// const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const isProduction = process.env.NODE_ENV == 'production';
-
-
 const stylesHandler = 'style-loader';
-
-
 
 const config = {
     entry: './src/index.js',
@@ -21,10 +15,6 @@ const config = {
         host: 'localhost',
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
@@ -56,10 +46,7 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
+        // config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
     } else {
         config.mode = 'development';
     }
