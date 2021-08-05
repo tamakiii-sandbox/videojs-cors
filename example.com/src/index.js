aaa1ce5a82eb5eafcf47199ede637de527e87966
tamakiii <tamakiii@users.noreply.github.com>
2021-08-05 11:48:45 +0900

    using dist/main.js

--- example.com/src/index.js
+++ example.com/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import videojs from 'video.js';
+import 'video.js/dist/video-js.min.css'
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.querySelector('video');
+    const player = videojs(video);
+    console.log(player);
+});
